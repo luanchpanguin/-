254ae0edd4b2d3c6ced8b87d763b0cbe1498b09b
luanch <49183429+luanchpanguin@users.noreply.github.com>
2021-05-12 15:15:02 +0800

    第一版

---
+++ ProjectOne/src/FirstProcess.java
@@ -0,0 +1,16 @@
+import java.io.Console;
+
+public class FirstProcess {
+
+	public static void main(String[] args) {
+		
+		// int 
+        System.out.println("基本类型：int 二进制位数：" + Integer.SIZE); 
+        System.out.println("包装类：java.lang.Integer"); 
+        System.out.println("最小值：Integer.MIN_VALUE=" + Integer.MIN_VALUE); 
+        System.out.println("最大值：Integer.MAX_VALUE=" + Integer.MAX_VALUE); 
+        System.out.println(); 
+          
+	}
+
+}
