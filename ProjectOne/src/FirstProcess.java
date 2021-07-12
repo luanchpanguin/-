ea0f7aeb90795ebf8dc206d4353b2ba5e2de798e
luanch <mit74@163.com>
2021-07-12 13:46:15 +0800

    Update FirstProcess.java
    修改第二版

--- ProjectOne/src/FirstProcess.java
+++ ProjectOne/src/FirstProcess.java
@@ -5,10 +5,10 @@ public class FirstProcess {
 	public static void main(String[] args) {
 		
 		// int 
-        System.out.println("基本类型：int 二进制位数：" + Integer.SIZE); 
-        System.out.println("包装类：java.lang.Integer"); 
-        System.out.println("最小值：Integer.MIN_VALUE=" + Integer.MIN_VALUE); 
-        System.out.println("最大值：Integer.MAX_VALUE=" + Integer.MAX_VALUE); 
+        System.out.println("鏁版嵁澶у皬" + Integer.SIZE); 
+        System.out.println("娴嬭瘯java.lang.Integer"); 
+        System.out.println("娴嬭瘯Integer.MIN_VALUE=" + Integer.MIN_VALUE); 
+        System.out.println("娴嬭瘯Integer.MAX_VALUE=" + Integer.MAX_VALUE); 
         System.out.println(); 
           
 	}
